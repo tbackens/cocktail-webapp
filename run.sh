d8f20c9897b66d6438c1cda032001ba2258443db
tbackens <99706186+tbackens@users.noreply.github.com>
2023-05-14 14:42:57 +0200

    added browser open to run.sh

--- run.sh
+++ run.sh
@@ -2,7 +2,8 @@
 ip=$(ifconfig | grep "inet " | grep -Fv 127.0.0.1 | awk '{print $2}') 
 #HOST=$ip npm start &
 npm start &
-chromium-browser --start-fullscreen ${ip}:3000 &
 cd server
-python3 api.py
+python3 api.py &
+sleep 15
+chromium-browser --start-fullscreen ${ip}:3000/qr
 
